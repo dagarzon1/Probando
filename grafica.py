d3fac4aaf5f554f624fe69c9a0e86dbf7108391e
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-03-15 07:39:35 -0500

    grafica

--- grafica.py
+++ grafica.py
@@ -1,5 +1,5 @@
 import matplotlib.pylab as plt
- 
+import numpy as np
 data=np.loadtxt("fecha_mancha.dat")
 plt.plot(data[:,0],data[:,1])
-plt.savefig("fecha_manchas.pdf")
+plt.savefig("fecha_manchas.pdf") 
